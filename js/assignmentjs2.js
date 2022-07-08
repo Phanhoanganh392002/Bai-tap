500a3b97c9370afafe4ee0a875161bbeed269806
Phanhoanganh392002 <dfang99999@gmail.com>
2022-07-08 10:55:48 +0700

    bai tap

---
+++ js/assignmentjs2.js
@@ -0,0 +1,21 @@
+// neu dang la do C thi set atrb role = none
+// F->C: F=100 -> (100-32)/1.8=37.78 do C
+/*
+cl[0].addEventListener('click', () => {
+    console.log("hello");
+    nd = Math.round((tp - 32) / 1.8);
+    temp.text(tp);
+    cl[0].setAttribute("disabled", "true");
+    cl[1].removeAttribute("disabled")
+});
+// chuyen do C thanh do F: C=50 -> 1.8*50+32=122 do F
+cl[1].addEventListener('click', () => {
+    nd = Math.round(1.8 * tp + 32);
+    temp.text(tp);
+    cl[1].setAttribute("disabled", "true");
+    cl[0].removeAttribute("disabled")
+});*/
+/*
+$(".tp").click(function() {
+   var nd= parseInt(temp.text());
+})*/
